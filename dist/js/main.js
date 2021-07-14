b96c517ae9512b3c589ba5d2462bc94869ce9176
sampanchan <spankratz@my.creativecircus.edu>
2021-07-14 17:15:34 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+"use strict";
+
+console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+console.log(` 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`)
