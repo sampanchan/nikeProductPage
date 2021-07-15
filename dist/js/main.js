c55a80b814102914c0beacbbfb4b5d96d3c5b2ac
sampanchan <spankratz@my.creativecircus.edu>
2021-07-15 13:42:31 -0400

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files."); // window.addEventListener('load', function() {
-//     document.querySelector('.loading').classList.add('hide')
-// })
+console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+window.addEventListener('load', function () {
+  document.querySelector('.loading').classList.add('hide');
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ Change this message, and make sure it changes in the browser
 to verify that you're working in the right files.`)
 
 
-// window.addEventListener('load', function() {
-//     document.querySelector('.loading').classList.add('hide')
-// })
+window.addEventListener('load', function() {
+    document.querySelector('.loading').classList.add('hide')
+})
