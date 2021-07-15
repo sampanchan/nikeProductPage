4df57bb4a0c8e3c76383a2c29a27249449bba38a
sampanchan <spankratz@my.creativecircus.edu>
2021-07-15 09:52:57 -0400

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 console.log(` 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+
+// window.addEventListener('load', function() {
+//     document.querySelector('.loading').classList.add('hide')
+// })
