30a9222e421a555a12ab94247a7685d82016ab82
sampanchan <spankratz@my.creativecircus.edu>
2021-07-22 11:08:20 -0400

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+document.querySelector('.fa-minus');
 window.addEventListener('load', function () {
   document.querySelector('.loading').classList.add('hide');
 });
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,14 @@ Change this message, and make sure it changes in the browser
 to verify that you're working in the right files.`)
 
 
+
 window.addEventListener('load', function() {
     document.querySelector('.loading').classList.add('hide')
 })
+
+let accordianButton = document.querySelector('.accordian-btn');
+
+for (let i = 0; i < accordianButton.length; i++){
+    accordionButton[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+}
