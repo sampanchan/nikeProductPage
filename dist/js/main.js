0333b1d2a85ea0e03beb97875aac119bb5c17549
sampanchan <spankratz@my.creativecircus.edu>
2021-07-22 11:36:55 -0400

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,14 @@
 "use strict";
 
 console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
-document.querySelector('.fa-minus');
 window.addEventListener('load', function () {
   document.querySelector('.loading').classList.add('hide');
 });
+var accordianButton = document.querySelector('.accordian-btn');
+
+for (var i = 0; i < accordianButton.length; i++) {
+  accordionButton[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
+}
 //# sourceMappingURL=main.js.map
