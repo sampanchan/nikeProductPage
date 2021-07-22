e07e12dbfd51152675a9671b9116f9d3a5b4c2b0
Shelly Graham <webdevshelly@gmail.com>
2021-07-22 12:45:58 -0400

    Accordion Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,25 @@
 console.log(` 
 Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+to verify that you're working in the right files.`);
 
-
-
-window.addEventListener('load', function() {
-    document.querySelector('.loading').classList.add('hide')
+window.addEventListener("load", function () {
+	document.querySelector(".loading").classList.add("hide");
 });
 
-let accordianButton = document.querySelector('.accordian-btn');
+let accordianButton = document.querySelector(".accordian-btn");
 
-for (let i = 0; i < accordianButton.length; i++){
-    accordionButton[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-})
+// Added by Shelly //
+for (let i = 0; i < accordionButtons.length; i++) {
+	accordionButtons[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+
+		let accordionContent = this.nextElementSibling;
+
+		if (accordionContent.style.maxHeight) {
+			accordionContent.style.maxHeight = null;
+		} else {
+			accordionContent.style.maxHeight =
+				accordionContent.scrollHeight + "px";
+		}
+	});
 }
