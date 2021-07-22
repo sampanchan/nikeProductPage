fbeb298b574d0be3e4f733c5daca90178bc44b68
sampanchan <spankratz@my.creativecircus.edu>
2021-07-22 12:42:41 -0400

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,12 +3,25 @@
 console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
 window.addEventListener('load', function () {
   document.querySelector('.loading').classList.add('hide');
-});
-var accordianButton = document.querySelector('.accordian-btn');
+}); // let accordianButton = document.querySelector('.accordian-btn');
+// for (let i = 0; i < accordianButton.length; i++){
+//     accordionButton[i].addEventListener('click', function() {
+//         this.classList.toggle('active');
+// })
+// }
 
-for (var i = 0; i < accordianButton.length; i++) {
-  accordionButton[i].addEventListener('click', function () {
+var accordionButtons = document.getElementsByClassName('accordian-item__button');
+
+for (var i = 0; i < accordionButtons.length; i++) {
+  accordionButtons[i].addEventListener('click', function () {
     this.classList.toggle('active');
+    var accordionContent = this.nextElementSibling;
+
+    if (accordionContent.style.maxHeight) {
+      accordionContent.style.maxHeight = null;
+    } else {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+    }
   });
 }
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,27 @@ window.addEventListener('load', function() {
     document.querySelector('.loading').classList.add('hide')
 });
 
-let accordianButton = document.querySelector('.accordian-btn');
+// let accordianButton = document.querySelector('.accordian-btn');
 
-for (let i = 0; i < accordianButton.length; i++){
-    accordionButton[i].addEventListener('click', function() {
+// for (let i = 0; i < accordianButton.length; i++){
+//     accordionButton[i].addEventListener('click', function() {
+//         this.classList.toggle('active');
+// })
+// }
+
+let accordionButtons = document.getElementsByClassName('accordion-item__button');
+
+
+for (let i = 0; i < accordionButtons.length; i++) {
+    accordionButtons[i].addEventListener('click', function() {
         this.classList.toggle('active');
-})
+        let accordionContent = this.nextElementSibling;
+        
+        if (accordionContent.style.maxHeight) {
+            accordionContent.style.maxHeight = null;
+        }
+         else {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        }
+    });
 }
